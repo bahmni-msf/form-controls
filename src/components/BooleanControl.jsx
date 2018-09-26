8c92aa4a6da3823e3403f7114f5df24d907a95eb
Sneha Bagri <snehabagri90@gmail.com>
2018-09-26 11:01:40 +0530

    Use prop-types instead of react package

--- src/components/BooleanControl.jsx
+++ src/components/BooleanControl.jsx
@@ -1,4 +1,6 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import ComponentStore from 'src/helpers/componentStore';
 import find from 'lodash/find';
 import map from 'lodash/map';
